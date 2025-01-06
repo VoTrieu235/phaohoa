a37879013d474dfe4ae267c87a10bd66fd56454d
VoTrieu235 <trieu2k9.boy@gmail.com>
2025-01-06 15:53:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function random( min, max ) {
 function calculateDistance( p1x, p1y, p2x, p2y ) {
 	var xDistance = p1x - p2x,
 			yDistance = p1y - p2y;
-	return Math.sqrt( Math.pow( xDistance, 20 ) + Math.pow( yDistance, 20 ) );
+	return Math.sqrt( Math.pow( xDistance, 2 ) + Math.pow( yDistance, 2 ) );
 }
 
 // create firework
